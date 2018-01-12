98b48616f99813130d8a2bc18ec6ef6644e6e552
Anaïs Payet <anais@impraise.com>
2018-01-12 12:28:03 +0100

    Update router templates

--- Clean Swift/Scene.xctemplate/UICollectionViewController/___FILEBASENAME___ViewController.swift
+++ Clean Swift/Scene.xctemplate/UICollectionViewController/___FILEBASENAME___ViewController.swift
@@ -19,7 +19,7 @@ protocol ___VARIABLE_sceneName___DisplayLogic: class {
 class ___VARIABLE_sceneName___ViewController: UICollectionViewController, ___VARIABLE_sceneName___DisplayLogic {
 
     var interactor: ___VARIABLE_sceneName___BusinessLogic?
-    var router: (NSObjectProtocol & ___VARIABLE_sceneName___RoutingLogic & ___VARIABLE_sceneName___DataPassing)?
+    var router: (___VARIABLE_sceneName___RoutingLogic & ___VARIABLE_sceneName___DataPassing)?
 
     // MARK: Object lifecycle
 
@@ -46,17 +46,6 @@ class ___VARIABLE_sceneName___ViewController: UICollectionViewController, ___VAR
         presenter.viewController = viewController
         router.viewController = viewController
         router.dataStore = interactor
-    }
-
-    // MARK: Routing
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let scene = segue.identifier {
-            let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-            if let router = router, router.responds(to: selector) {
-                router.perform(selector, with: segue)
-            }
-        }
     }
 
     // MARK: View lifecycle
